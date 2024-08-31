7c09cf274abedc622ff856a6b0713a3185b2b932
miikuru002 <jamutaqortegavelez@gmail.com>
2024-08-31 14:11:56 -0500

    add: base project

---
+++ src/main/java/com/verysafe/falconshield/FalconShieldApplication.java
@@ -0,0 +1,13 @@
+package com.verysafe.falconshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalconShieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FalconShieldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/verysafe/falconshield/FalconShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verysafe.falconshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalconShieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
