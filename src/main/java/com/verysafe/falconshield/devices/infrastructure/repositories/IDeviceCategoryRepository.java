acc38fb49a40b176f42a53cc1a3b95e67cbb68de
miikuru002 <jamutaqortegavelez@gmail.com>
2024-10-05 20:33:43 -0500

    add: device entities & repos

---
+++ src/main/java/com/verysafe/falconshield/devices/infrastructure/repositories/IDeviceCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.verysafe.falconshield.devices.infrastructure.repositories;
+
+import com.verysafe.falconshield.devices.domain.model.entities.DeviceCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDeviceCategoryRepository extends JpaRepository<DeviceCategory, Long> {
+//    List<DeviceCategory> findAllByUserProfileAccountId(String accountId);
+}
---
+++ src/main/java/com/verysafe/falconshield/devices/infrastructure/repositories/IDeviceRepository.java
@@ -0,0 +1,10 @@
+package com.verysafe.falconshield.devices.infrastructure.repositories;
+
+import com.verysafe.falconshield.devices.domain.model.entities.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDeviceRepository extends JpaRepository<Device, String> {
+//    List<Device> findAllByUserProfileAccountId(String accountId);
+}
