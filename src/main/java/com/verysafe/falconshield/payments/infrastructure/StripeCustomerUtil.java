d1f9da5c8204d7201131928320426dca5c63962c
miikuru002 <jamutaqortegavelez@gmail.com>
2024-10-20 13:24:19 -0500

    add: stripe customer util

---
+++ src/main/java/com/verysafe/falconshield/payments/infrastructure/StripeCustomerUtil.java
@@ -0,0 +1,32 @@
+package com.verysafe.falconshield.payments.infrastructure;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.CustomerSearchParams;
+
+public class StripeCustomerUtil {
+    public static Customer findOrCreateCustomer(String email, String name) throws StripeException {
+        var params = CustomerSearchParams
+                .builder()
+                .setQuery(String.format("email:'%s'", email))
+                .build();
+
+        var result = Customer.search(params);
+
+        Customer customer;
+
+        //si no existe el cliente, se crea uno nuevo
+        if (result.getData().isEmpty()) {
+            var customerCreateParams = CustomerCreateParams.builder()
+                    .setName(name)
+                    .setEmail(email)
+                    .build();
+            customer = Customer.create(customerCreateParams);
+        } else {
+            customer = result.getData().getFirst();
+        }
+
+        return customer;
+    }
+}
