ac605e0fb4fc2557a4f3b99b9217968452d1ab89
GabySol26 <U202113876@upc.edu.pe>
2024-10-28 22:52:37 -0500

    add:entities area

---
+++ src/main/java/com/verysafe/falconshield/areas/domain/model/entities/Area.java
@@ -0,0 +1,29 @@
+package com.verysafe.falconshield.areas.domain.model.entities;
+import com.verysafe.falconshield.areas.domain.model.entities.AreaRegistration;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name="areas")
+public class Area{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String icon;
+
+    @Column(nullable = true)
+    private String color;
+
+    @OneToMany
+    private Set <AreaRegistration> registrations = new HashSet<>();
+}
---
+++ src/main/java/com/verysafe/falconshield/areas/domain/model/entities/AreaRegistration.java
@@ -0,0 +1,34 @@
+import com.verysafe.falconshield.areas.domain.model.entities.AreaRegistration;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name="area_registrations")
+public class AreaRegistration{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="area_id")
+    private Area area;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    //@OneToMany
+    //@JoinColumn(name = "users_devices_id")
+    //private UserDevice users_devices_id;
+
+    @CreationTimestamp
+    private LocalDateTime registeredAt;
+
+    public AreaRegistration(){}
+
+}
