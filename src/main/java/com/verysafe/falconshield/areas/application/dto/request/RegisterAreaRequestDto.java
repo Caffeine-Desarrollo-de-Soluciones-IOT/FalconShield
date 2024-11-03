aa42a2013fa4cf3649e8425d4482a86b290a10b1
miikuru002 <jamutaqortegavelez@gmail.com>
2024-11-03 02:41:39 -0500

    fix: register area dto

--- src/main/java/com/verysafe/falconshield/areas/application/dto/request/RegisterAreaRequestDto.java
+++ src/main/java/com/verysafe/falconshield/areas/application/dto/request/RegisterAreaRequestDto.java
@@ -1,19 +1,11 @@
 package com.verysafe.falconshield.areas.application.dto.request;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record RegisterAreaRequestDto(
-    @NotNull(message = "Area ID is required")
-    @Min(value = 1, message = "Area ID must be greater than 0")
-    Long areaId,
-
     @NotEmpty(message = "Name is required" )
     String name,
 
-    @NotEmpty(message = "Icon is required")
     String icon,
 
-    @NotEmpty(message = "Color is required" )
     String color
 ) {}
